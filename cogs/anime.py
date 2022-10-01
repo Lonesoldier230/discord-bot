815be41fbbae0de9fd5105bba2096f928d2a5ecb
Lonesoldier230 <98001588+Lonesoldier230@users.noreply.github.com>
2022-10-01 21:08:24 +0545

    color updates

--- cogs/anime.py
+++ cogs/anime.py
@@ -1,7 +1,16 @@
 import disnake
 from disnake.ext import commands  
 import requests
+import random
 import json
+
+
+def color():
+    # this program choose colors for the embed randomly
+    color = (0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f,
+             0xc27c0e, 0xe67e22, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a, 0x7289da, 0x99aab5)
+    randclr = random.choice(color)
+    return randclr
 
 def search(type,name=None):
     if type == "anime":
@@ -61,7 +70,7 @@ def embeder(result,name,type=None):
         synopsis = ".".join(shortner(result[name]["synopsis"]))
     else:
         synopsis = None
-    embed = disnake.Embed(title=name, url=url, color=0xFF5733)
+    embed = disnake.Embed(title=name, url=url, color=color())
     embed.set_thumbnail(url=result[name]["image"])
     embed.add_field(name=":regional_indicator_g: Genre",value=result[name]["genre"], inline=False)
     embed.add_field(name=":100: Score", value=result[name]["score"], inline=True)
